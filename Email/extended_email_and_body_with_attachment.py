70c7f14a3b84f82572c2c6d43919d5cb9a5c4f56
Dombold <dominic_bolding@hotmail.com>
2016-06-11 21:38:48 +0800

    Add files via upload

---
+++ Email/extended_email_and_body_with_attachment.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+
+fromaddr = "FromUser@gmail.com"
+toaddr = "ToUser@gmail.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Test Alert"
+f = open("PathToEmailBodyTextWithExtension")
+
+body = f.read()
+msg.attach(MIMEText(body, 'plain'))
+
+filename = "AttachmentFileWithExt"
+attachment = open("PathToFile", "rb")
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+msg.attach(part)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "FromUserEmailPassword")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
