b67f46dcd63a997ce80d56ebbb344cb9d6540111
Dombold <dominic_bolding@hotmail.com>
2016-06-17 22:20:53 +0800

    Update extended_email_and_body_with_attachment_multiple_recipients.py

--- Email/extended_email_and_body_with_attachment_multiple_recipients.py
+++ Email/extended_email_and_body_with_attachment_multiple_recipients.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
 from email import encoders
 
 fromaddr = "FromUser@gmail.com"
