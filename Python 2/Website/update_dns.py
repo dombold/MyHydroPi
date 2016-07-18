dbef8c1306f4832c35d22dcb884bb5faa51b5717
Dombold <dominic_bolding@hotmail.com>
2016-07-18 20:34:20 +0800

    Update update_dns.py

--- Python 2/Website/update_dns.py
+++ Python 2/Website/update_dns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import urllib2
+from urllib2 import urlopen
 
-urllib2.urlopen("https://dynamicdns.park-your-domain.com/update?"
-                "host={}"
-                "&domain={}"
-                "&password={}"
-                .format("@", "Your_Domain_Name", "Your_Namecheap_Dynamic_DNS_Passwd"))
+urlopen("https://dynamicdns.park-your-domain.com/update?"
+        "host={}"
+        "&domain={}"
+        "&password={}"
+        .format("@", "Your_Domain_Name", "Your_Namecheap_Dynamic_DNS_Passwd"))
