be0bb084f78884c409b2c723c7434f56431f2bc4
Dombold <dominic_bolding@hotmail.com>
2016-09-26 17:26:44 +0800

    Update num_dt_pairs.php

--- Hydropi_WebSite/php/num_dt_pairs.php
+++ Hydropi_WebSite/php/num_dt_pairs.php
@@ -2,7 +2,7 @@
 // NUM_DT_PAIRS.PHP
     require "hydropi_connect.php";
     // Count the number of datetime pairs associated with the relay
-    $sql= ("SELECT * FROM ".$title."");
+    $sql= ("SELECT * FROM ".$relaynames[$x]."");
     if ($result = mysqli_query($conn, $sql)) {
         $dt_count = mysqli_num_rows($result);
     mysqli_free_result($result);
