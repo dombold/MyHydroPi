0c57b188efc81fbdb514f27672314777fc0ee55f
Dombold <dominic_bolding@hotmail.com>
2016-09-26 17:21:22 +0800

    Update relay_table_names.php

--- Hydropi_WebSite/php/relay_table_names.php
+++ Hydropi_WebSite/php/relay_table_names.php
@@ -4,14 +4,17 @@
     // Get all the connected relay names and place in an array
     $sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'hydropi'";
     $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
+    $row_cnt = mysqli_num_rows($result);
+    if ($row_cnt > 0) {
         // output data of each row
         $relaynames = array();
-        $relaynum = 0;
         while($row = mysqli_fetch_assoc($result)) {
-            $relaynum = $relaynum + 1;
-            if ($row['TABLE_NAME'] == ("relay_" .$relaynum. "_timer")) {
-                array_push($relaynames, $row['TABLE_NAME']);
+            $relaynum = 0;
+            for ($x = 0; $x < $row_cnt; $x++) {
+                $relaynum = $relaynum + 1;
+                if ($row['TABLE_NAME'] == ("relay_" .$relaynum. "_timer")) {
+                    $relaynames[$relaynum] = $row['TABLE_NAME'];
+                }
             }
         }
     }
