f3d78f0ccabc23c7ea5619253711cc03d9c6a6aa
Dominic Bolding <dominic_bolding@hotmail.com>
2019-07-22 19:50:33 +0800

    Added Email code

--- Python 3/Email/ext_email_ext_body_with_attach_csv_multi_receivers_py3.py
+++ Python 3/Email/ext_email_ext_body_with_attach_csv_multi_receivers_py3.py
@@ -10,14 +10,17 @@
 # Please feel free to use and modify this code for you own use.
 #
 # This program is designed to send an email with an attachment, sender,
-# receiver and a subject line. In addition it will also read a pre prepared
-# text and html file and use it's contents to create the body of the email
+# receivers and a subject line. In addition it will also read a pre prepared
+# text and html file and use it's contents to create the body of the email.
+# Finally it will use a CSV file to get the receivers and send them a
+# personalised email.
 #
 ##############################################################################
 
 #!/usr/bin/env python
 
 import smtplib
+import csv
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -26,23 +29,23 @@ from email import encoders
 
 # Create email header
 fromaddr = "FromUser@gmail.com"
-toaddr = "ToUser1@gmail.com,ToUser2@gmail.com,ToUser3@gmail.com"
-alladdr = toaddr.split(",")
 
-msg = MIMEMultipart()
+msg = MIMEMultipart("mixed")
 msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Test Alert"
+msg['Subject'] = "Final Results"
+
+msgAlternative = MIMEMultipart('alternative')
+msg.attach(msgAlternative)
 
 # Open the body text file and join it to the message
 ftext = open("FullPathToEmailBodyText/FileName.Extension")
 bodytext = ftext.read()
-msg.attach(MIMEText(bodytext, 'plain'))
+msgAlternative.attach(MIMEText(bodytext, 'plain'))
 
 # Open the body html file and join it to the message
 fhtml = open("FullPathToEmailBodyHTML/FileName.Extension")
 bodyhtml = fhtml.read()
-msg.attach(MIMEText(bodyhtml, 'html'))
+msgAlternative.attach(MIMEText(bodyhtml, 'html'))
 
 # Open the attachment and join it to the message
 filename = "AttachmentFileName"
@@ -58,9 +61,16 @@ msg.attach(part)
 # Establish connection with the email server
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, "FromUserEmailPassword")
+server.login(fromaddr, "FromUser_Email_Password")
 
 # Join all parts of the email and send
-text = msg.as_string()
-server.sendmail(fromaddr, alladdr, text)
+with open("FullPathToCsvFile/FileName.Extension") as file:
+    reader = csv.reader(file)
+    next(reader)
+    # Read the CSV file to get receiver emails and personalise
+    # the body of the email
+    for User, Email, Score, Result in reader:
+        text = msg.as_string()
+        text = text.format(User=User,Score=Score,Result=Result)
+        server.sendmail(fromaddr, Email, text,)
 server.quit()
