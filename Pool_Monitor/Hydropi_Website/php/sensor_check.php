c0c21c29d7a798bcd717ecdfb3fbad7d89e62740
Dominic Bolding <dominic_bolding@hotmail.com>
2019-08-21 21:01:02 +0800

    Update Sensor Check.php

--- Pool_Monitor/Hydropi_Website/php/sensor_check.php
+++ Pool_Monitor/Hydropi_Website/php/sensor_check.php
@@ -54,8 +54,10 @@
                             echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],1)." &degC\";\n";
                             $check = mysqli_query($conn, ("SELECT ds18b20_temp_low FROM settings WHERE pk=1"));
                             $value = mysqli_fetch_row($check);
+                            $gettemp1 = mysqli_query($conn, ("SELECT ".$title." FROM `sensors` WHERE 1 ORDER BY timestamp DESC LIMIT 1"));
+                            $currtemp1 = mysqli_fetch_row($gettemp1);
                             // Present a recommended action based on the average values returned
-                            if(round($row[0],1) < ($value[0])){
+                            if(round($currtemp1[0],0) < ($value[0])){
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Too Cold for a Swim\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
@@ -66,6 +68,7 @@
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-success\");\n";
                             }
                             mysqli_free_result($check);
+                            mysqli_free_result($gettemp1);
                         }
                         elseif ($title == "atlas_temp") {
                             echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],1)." &degC\";\n";
@@ -73,13 +76,15 @@
                             $value_low = mysqli_fetch_row($check);
                             $check2 = mysqli_query($conn, ("SELECT atlas_temp_hi FROM settings WHERE pk=1"));
                             $value_high = mysqli_fetch_row($check2);
-                            if(round($row[0],1) < ($value_low[0])){
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Put the cover on\";\n";
+                            $gettemp2 = mysqli_query($conn, ("SELECT ".$title." FROM `sensors` WHERE 1 ORDER BY timestamp DESC LIMIT 1"));
+                            $currtemp2 = mysqli_fetch_row($gettemp2);
+                            if(round($currtemp2[0],0) < ($value_low[0])){
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Put the Cover On\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
-                            elseif(round($row[0],1) > ($value_high[0])){
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Take the cover off\";\n";
+                            elseif(round($currtemp2[0],0) > ($value_high[0])){
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Take the Cover Off\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
@@ -90,6 +95,7 @@
                             }
                             mysqli_free_result($check);
                             mysqli_free_result($check2);
+                            mysqli_free_result($gettemp2);
                         }
                         elseif ($title == "ph") {
                             echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],2)."\";\n";
@@ -99,13 +105,13 @@
                             $value_high = mysqli_fetch_row($check2);
                             if ((round($row[0],1) < ($value_low[0]))) {
                                 $ph_inc_value = (((7.4 - (round($row[0],1)))/0.1)*2)*($pool_vol[0]/1000);
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_inc_value." g of pH Inc\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_inc_value."g of pH Increaser\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             elseif ((round($row[0],1) > ($value_high[0]))) {
                                 $ph_hcl = ((round($row[0],1)) - 7.1)*$pool_vol[0]*0.03;
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_hcl." ml of Acid\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_hcl."ml of Acid\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
@@ -123,13 +129,14 @@
                             $value_low = mysqli_fetch_row($check);
                             $check2 = mysqli_query($conn, ("SELECT orp_hi FROM settings WHERE pk=1"));
                             $value_high = mysqli_fetch_row($check2);
+                            $add_chlorine = round(((650 - $row[0])/1.666),0);
                             if ((round($row[0],0) < ($value_low[0]))) {
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add 100 g of Chlorine\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$add_chlorine."g of Chlorine\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             elseif ((round($row[0],0) > ($value_high[0]))) {
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Turn down chlorinator and remove cover\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Turn Down Chlorinator / Remove Cover\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
@@ -151,12 +158,12 @@
                                 $salt_start = ($pool_vol[0]*($row[0]))/1000000;
                                 $salt_finish = ($pool_vol[0]*5500)/1000000;
                                 $add_salt = round(($salt_finish - $salt_start),0);
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$add_salt." kg of salt\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$add_salt."kg of Salt\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             elseif ((round($row[0],0) > ($value_high[0]))) {
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add fresh water\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add Fresh Water\";\n";
                                 echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
                                 echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
--- Pool_Monitor/Hydropi_Website/php/sensor_check_24hr.php
+++ Pool_Monitor/Hydropi_Website/php/sensor_check_24hr.php
@@ -32,45 +32,51 @@
                     if($result = mysqli_query($conn, $stmt)){
                         $row = mysqli_fetch_row($result);
                         if ($title == "ds18b20_temp") {
-                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],1)."<sup>&degC</sup>\";\n";
+                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],1)." &degC\";\n";
                             $check = mysqli_query($conn, ("SELECT ds18b20_temp_low FROM settings WHERE pk=1"));
                             $value = mysqli_fetch_row($check);
+                            $gettemp1 = mysqli_query($conn, ("SELECT ".$title." FROM `sensors` WHERE 1 ORDER BY timestamp DESC LIMIT 1"));
+                            $currtemp1 = mysqli_fetch_row($gettemp1);
                             // Present a recommended action based on the average values returned
-                            if(round($row[0],1) < ($value[0])){
+                            if(round($currtemp1[0],0) < ($value[0])){
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Too Cold for a Swim\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             else {
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"OK\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#88c149\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-success\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-success\");\n";
                             }
                             mysqli_free_result($check);
+                            mysqli_free_result($gettemp1);
                         }
                         elseif ($title == "atlas_temp") {
-                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],1)."<sup>&degC</sup>\";\n";
+                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],1)." &degC\";\n";
                             $check = mysqli_query($conn, ("SELECT atlas_temp_low FROM settings WHERE pk=1"));
                             $value_low = mysqli_fetch_row($check);
                             $check2 = mysqli_query($conn, ("SELECT atlas_temp_hi FROM settings WHERE pk=1"));
                             $value_high = mysqli_fetch_row($check2);
-                            if(round($row[0],1) < ($value_low[0])){
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Put the cover on\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                            $gettemp2 = mysqli_query($conn, ("SELECT ".$title." FROM `sensors` WHERE 1 ORDER BY timestamp DESC LIMIT 1"));
+                            $currtemp2 = mysqli_fetch_row($gettemp2);
+                            if(round($currtemp2[0],0) < ($value_low[0])){
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Put the Cover On\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
-                            elseif(round($row[0],1) > ($value_high[0])){
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Take the cover off\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                            elseif(round($currtemp2[0],0) > ($value_high[0])){
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Take the Cover Off\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             else {
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"OK\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#88c149\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-success\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-success\");\n";
                             }
                             mysqli_free_result($check);
                             mysqli_free_result($check2);
+                            mysqli_free_result($gettemp2);
                         }
                         elseif ($title == "ph") {
                             echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],2)."\";\n";
@@ -80,50 +86,51 @@
                             $value_high = mysqli_fetch_row($check2);
                             if ((round($row[0],1) < ($value_low[0]))) {
                                 $ph_inc_value = (((7.4 - (round($row[0],1)))/0.1)*2)*($pool_vol[0]/1000);
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_inc_value." g of pH Inc\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_inc_value."g of pH Increaser\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             elseif ((round($row[0],1) > ($value_high[0]))) {
                                 $ph_hcl = ((round($row[0],1)) - 7.1)*$pool_vol[0]*0.03;
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_hcl." ml of Acid\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$ph_hcl."ml of Acid\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             else {
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"OK\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#88c149\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-success\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-success\");\n";
                             }
                             mysqli_free_result($check);
                             mysqli_free_result($check2);
                         }
                         elseif ($title == "orp") {
-                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],0)."mV\";\n";
+                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],0)." mV\";\n";
                             $check = mysqli_query($conn, ("SELECT orp_low FROM settings WHERE pk=1"));
                             $value_low = mysqli_fetch_row($check);
                             $check2 = mysqli_query($conn, ("SELECT orp_hi FROM settings WHERE pk=1"));
                             $value_high = mysqli_fetch_row($check2);
+                            $add_chlorine = round(((650 - $row[0])/1.666),0);
                             if ((round($row[0],0) < ($value_low[0]))) {
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add 100 g of Chlorine\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$add_chlorine."g of Chlorine\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             elseif ((round($row[0],0) > ($value_high[0]))) {
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Reduce chlorinator / remove cover\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Turn Down Chlorinator / Remove Cover\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             else {
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"OK\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#88c149\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-success\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-success\");\n";
                             }
                             mysqli_free_result($check);
                             mysqli_free_result($check2);
                         }
                         elseif ($title == "ec") {
-                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],0)."ppm\";\n";
+                            echo  "document.getElementById(\"".$title."_avg\").innerHTML = \"".round($row[0],0)." ppm\";\n";
                             $check = mysqli_query($conn, ("SELECT ec_low FROM settings WHERE pk=1"));
                             $value_low = mysqli_fetch_row($check);
                             $check2 = mysqli_query($conn, ("SELECT ec_hi FROM settings WHERE pk=1"));
@@ -132,19 +139,19 @@
                                 $salt_start = ($pool_vol[0]*($row[0]))/1000000;
                                 $salt_finish = ($pool_vol[0]*5500)/1000000;
                                 $add_salt = round(($salt_finish - $salt_start),0);
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$add_salt." kg of salt\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add ".$add_salt."kg of Salt\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             elseif ((round($row[0],0) > ($value_high[0]))) {
-                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add fresh water\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#e12b31\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").innerHTML = \"Add Fresh Water\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-danger\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-danger\");\n";
                             }
                             else {
                                 echo  "document.getElementById(\"".$title."_check\").innerHTML = \"OK\";\n";
-                                echo  "document.getElementById(\"".$title."_check\").style.color = \"#FFFFFF\";\n";
-                                echo  "document.getElementById(\"".$title."_color\").style.backgroundColor = \"#88c149\";\n";
+                                echo  "document.getElementById(\"".$title."_check\").classList.add (\"bg-success\");\n";
+                                echo  "document.getElementById(\"".$title."_name\").classList.add (\"bg-success\");\n";
                             }
                             mysqli_free_result($check);
                             mysqli_free_result($check2);
@@ -152,7 +159,6 @@
 
                     }
                 }
-            break;
 
     mysqli_free_result($result);
     mysqli_close($conn);
